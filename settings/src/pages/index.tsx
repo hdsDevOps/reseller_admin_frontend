1e8be4ba319c72ab030dbc48d5806b1e5b6f5549
el-collins <charlescollins476@gmail.com>
2024-11-04 10:47:44 +0100

    💡 Finished customer agreement UI and Terms & conditions UI : 2024-11-04 09:47:44 💡

--- settings/src/pages/index.tsx
+++ settings/src/pages/index.tsx
@@ -1,5 +1,7 @@
 import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
+import CustomerAgreement from "../components/CustomerAgreement";
+import TermsConditions from "../components/TermsConditions";
 
 
 const Settings = React.lazy(() => import("./Settings"));
@@ -10,6 +12,8 @@ const SettingsApp: React.FC = () => {
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/settings" element={<Settings />} />
+        <Route path="/customer-agreement" element={<CustomerAgreement />} />
+        <Route path="/terms-and-conditions" element={<TermsConditions />} />
       </Routes>
     </Suspense>
   );
