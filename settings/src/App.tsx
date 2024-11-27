c01ba2d4e6dd9f5f477a8bff602c13136cb01f85
saunak99 <saunak99@gmail.com>
2024-11-27 13:04:05 +0530

    changes in deployment

--- settings/src/App.tsx
+++ settings/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.scss'
-
+ 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
     <div>Name: settings</div>
