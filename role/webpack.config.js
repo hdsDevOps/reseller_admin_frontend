dce1e634d9b49634f7790051176e9eeb6d170b86
saunak99 <saunak99@gmail.com>
2024-11-27 19:34:58 +0530

    changes in code to deploy

--- role/webpack.config.js
+++ role/webpack.config.js
@@ -2,42 +2,20 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const Dotenv = require("dotenv-webpack");
 const deps = require("./package.json").dependencies;
-const printCompilationMessage = require('./compilation.config.js');
 module.exports = (_, argv) => ({
 	output: {
 		publicPath: "auto",
 	},
-     
+
 	resolve: {
 		extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
 	},
-
-	// devServer: {
-	// 	port: 3007,
-	// 	historyApiFallback: true,
-	// 	allowedHosts: ["all"],
-	// },
 
 	devServer: {
 		port: 3010,
 		historyApiFallback: true,
 		allowedHosts: ["all"],
-		onListening: function (devServer) {
-		  const port = devServer.server.address().port
-	
-		  printCompilationMessage('compiling', port)
-	
-		  devServer.compiler.hooks.done.tap('OutputMessagePlugin', (stats) => {
-			setImmediate(() => {
-			  if (stats.hasErrors()) {
-				printCompilationMessage('failure', port)
-			  } else {
-				printCompilationMessage('success', port)
-			  }
-			})
-		  })
-		}
-	  },
+	},
 
 	module: {
 		rules: [
