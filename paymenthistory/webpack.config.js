f5f31a0a658a0ac03205d8d68bf8c3c4058dc127
saunak99 <saunak99@gmail.com>
2024-11-27 16:36:07 +0530

    changes in code to deploy

--- paymenthistory/webpack.config.js
+++ paymenthistory/webpack.config.js
@@ -9,7 +9,7 @@ const printCompilationMessage = require('./compilation.config.js');
 
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:3005/",
+    publicPath: "auto",
   },
 
   resolve: {
