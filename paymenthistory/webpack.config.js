ffda021ee00a72f2b9689f5a5a5ca8ef42d35249
saunak99 <saunak99@gmail.com>
2024-11-28 18:13:17 +0530

    changes in customer file

--- paymenthistory/webpack.config.js
+++ paymenthistory/webpack.config.js
@@ -1,90 +1,91 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const Dotenv = require('dotenv-webpack');
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
 const deps = require("./package.json").dependencies;
+
 const printCompilationMessage = require('./compilation.config.js');
+
 module.exports = (_, argv) => ({
-	output: {
-		publicPath: "auto",
-	},
+  output: {
+    publicPath: "auto",
+  },
 
-	resolve: {
-		extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
-	},
-     
-	devServer: {
-		port: 4005,
-		historyApiFallback: true,
-		allowedHosts: ["all"],
-		watchFiles: [path.resolve(__dirname, 'src')],
-		onListening: function (devServer) {
-		const port = devServer.server.address().port
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
 
-		printCompilationMessage('compiling', port)
+  devServer: {
+    port: 4005,
+    historyApiFallback: true,
+    allowedHosts: ["all"],
+    watchFiles: [path.resolve(__dirname, 'src')],
+    onListening: function (devServer) {
+      const port = devServer.server.address().port
 
-		devServer.compiler.hooks.done.tap('OutputMessagePlugin', (stats) => {
-			setImmediate(() => {
-			if (stats.hasErrors()) {
-				printCompilationMessage('failure', port)
-			} else {
-				printCompilationMessage('success', port)
-			}
-			})
-		})
-		}
-	},
+      printCompilationMessage('compiling', port)
 
-	module: {
-		rules: [
-			{
-				test: /\.m?js/,
-				type: "javascript/auto",
-				resolve: {
-					fullySpecified: false,
-				},
-			},
-			{
-				test: /\.(css|s[ac]ss)$/i,
-				use: ["style-loader", "css-loader", "postcss-loader"],
-			},
-			{
-				test: /\.(ts|tsx|js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader",
-				},
-			},
-		],
-	},
+      devServer.compiler.hooks.done.tap('OutputMessagePlugin', (stats) => {
+        setImmediate(() => {
+          if (stats.hasErrors()) {
+            printCompilationMessage('failure', port)
+          } else {
+            printCompilationMessage('success', port)
+          }
+        })
+      })
+    }
+  },
 
-	plugins: [
-		new ModuleFederationPlugin({
-			name: "customer",
-			filename: "remoteEntry.js",
-			remotes: {
-				store: "store@https://store.admin.gworkspace.withhordanso.com/remoteEntry.js",
-			},
-			exposes: {
-				"./Cart": "./src/components/Cart.tsx",
-				"./CustomerApp": "./src/pages/index.tsx",
-				"./Css": "./src/custom.css",
-			},
-			shared: {
-				...deps,
-				react: {
-					singleton: true,
-					requiredVersion: deps.react,
-				},
-				"react-dom": {
-					singleton: true,
-					requiredVersion: deps["react-dom"],
-				},
-			},
-		}),
-		new HtmlWebPackPlugin({
-			template: "./src/index.html",
-		}),
-		new Dotenv(),
-	],
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "paymenthistory",
+      filename: "remoteEntry.js",
+      remotes: {
+        store: `store@${process.env.STORE_BASE_URL || 'http://localhost:3030'}/remoteEntry.js`,
+      },
+      exposes: {
+        "./PaymentApp": "./src/pages/index.tsx",
+      },
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: deps["react-dom"],
+        },
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+    new Dotenv()
+  ],
 });
