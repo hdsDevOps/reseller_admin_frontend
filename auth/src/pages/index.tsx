df55cede9d89d89232bdef13d37d06a3528c1851
saunak99 <saunak99@gmail.com>
2024-11-28 16:19:56 +0530

    changes in auth file

--- auth/src/pages/index.tsx
+++ auth/src/pages/index.tsx
@@ -1,24 +1,33 @@
 import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 
+const Header = React.lazy(() => import("../components/Heaader"));
+const Home = React.lazy(() => import("./Home"));
+const Login = React.lazy(() => import("./Login"));
+const Register = React.lazy(() => import("./Register"));
+const OTP = React.lazy(() => import("./OTP"));
+const ForgotPassword = React.lazy(() => import("./ForgotPassword"));
 
-const CustomerManagement = React.lazy(() => import("./CustomerManagement"));
-const CustomerInformation = React.lazy(() => import("./CustomerInformation"));
-const AddCustomer = React.lazy(() => import("./AddCustomer"));
-const EditCustomer = React.lazy(() => import("./EditCustomer"));
-
-
-const CustomerApp: React.FC = () => {
+const AuthApp: React.FC = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        <Route path="/customers" element={<CustomerManagement />} />
-        <Route path="/customer-information" element={<CustomerInformation />} />
-        <Route path="/add-customer" element={<AddCustomer />} />
-        <Route path="/edit-customer" element={<EditCustomer />} />
-      </Routes>
-    </Suspense>
+    <div className="min-h-screen bg-gray-100">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <Suspense fallback={<div>Loading...</div>}>
+          <Header />
+          <main className="py-10">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/otp" element={<OTP />} />
+              <Route path="/forgotpassword" element={<ForgotPassword />} />
+            </Routes>
+          </main>
+        </Suspense>
+      </div>
+    </div>
   );
 };
 
-export default CustomerApp;
+export default AuthApp;
--- auth/webpack.config.js
+++ auth/webpack.config.js
@@ -1,14 +1,12 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const Dotenv = require('dotenv-webpack');
+const Dotenv = require("dotenv-webpack");
 const deps = require("./package.json").dependencies;
-const printCompilationMessage = require('./compilation.config.js');
-const path = require('path');   
 module.exports = (_, argv) => ({
 	output: {
 		publicPath: "auto",
 	},
-        
+
 	resolve: {
 		extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
 	},
@@ -17,22 +15,6 @@ module.exports = (_, argv) => ({
 		port: 4001,
 		historyApiFallback: true,
 		allowedHosts: ["all"],
-		watchFiles: [path.resolve(__dirname, 'src')],
-		onListening: function (devServer) {
-		const port = devServer.server.address().port
-
-		printCompilationMessage('compiling', port)
-
-		devServer.compiler.hooks.done.tap('OutputMessagePlugin', (stats) => {
-			setImmediate(() => {
-			if (stats.hasErrors()) {
-				printCompilationMessage('failure', port)
-			} else {
-				printCompilationMessage('success', port)
-			}
-			})
-		})
-		}
 	},
 
 	module: {
@@ -66,9 +48,8 @@ module.exports = (_, argv) => ({
 				store: "store@https://store.admin.gworkspace.withhordanso.com/remoteEntry.js",
 			},
 			exposes: {
-				"./Cart": "./src/components/Cart.tsx",
-				"./CustomerApp": "./src/pages/index.tsx",
-				"./Css": "./src/custom.css",
+				"./AuthApp": "./src/pages/index.tsx",
+				"./AuthCss": "./src/auth.css",
 			},
 			shared: {
 				...deps,
