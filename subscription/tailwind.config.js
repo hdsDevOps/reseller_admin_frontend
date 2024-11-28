5d9fdbfb76a3fea08c37d0f03c1aef386e1578ee
saunak99 <saunak99@gmail.com>
2024-11-28 10:30:30 +0530

    changes in gitignore file

--- subscription/tailwind.config.js
+++ subscription/tailwind.config.js
@@ -1,42 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: [],
   theme: {
-    extend: {
-      fontFamily: {
-        inter: ["Inter", "sans-serif"],
-        roboto: ["Roboto", "sans-serif"],
-        montsettat: ["Montserrat", "sans-serif"],
-      },
-      colors: {
-        cGreen: '#12A833',
-        cGreen2: '#CDE9D3',
-        cGreen3: '#F3FAF7',
-        cGreen4: '#ECFFF0',
-        cBlue: '#0084FF',
-        cBlue2: '#0066FF',
-        cBlue3: '#0D121F',
-        cBlue4: '#0084ff33',
-        cBlue5: '#2B41FF',
-        cBlue6: '#F7FAFF',
-        cWhite: '#E4E4E4',
-        cWhite2: '#F9F9F9',
-        cWhite3: '#F4F4F4',
-        cWhite4: '#F0F0F3',
-        cWhite5: '#F9FAFB',
-        cRed: '#E02424',
-        cRed2: '#F9DEDC',
-        cGray: '#8A8A8A',
-        cGray2: '#777777',
-        cBlack: '#151515',
-        cBlack2: '#0D121F',
-        cBlack3: '#0E0E0E',
-        
-      }
-    },
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
 }
+
--- subscription/webpack.config.js
+++ subscription/webpack.config.js
@@ -1,44 +1,21 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const Dotenv = require("dotenv-webpack");
+const Dotenv = require('dotenv-webpack');
 const deps = require("./package.json").dependencies;
-const printCompilationMessage = require('./compilation.config.js');
 module.exports = (_, argv) => ({
 	output: {
 		publicPath: "auto",
 	},
-     
+
 	resolve: {
 		extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
 	},
 
-	// devServer: {
-	// 	port: 3007,
-	// 	historyApiFallback: true,
-	// 	allowedHosts: ["all"],
-	// },
-
 	devServer: {
-		port: 3011,
+		port: 4004,
 		historyApiFallback: true,
 		allowedHosts: ["all"],
-		watchFiles: [path.resolve(__dirname, 'src')],
-		onListening: function (devServer) {
-		  const port = devServer.server.address().port
-	
-		  printCompilationMessage('compiling', port)
-	
-		  devServer.compiler.hooks.done.tap('OutputMessagePlugin', (stats) => {
-			setImmediate(() => {
-			  if (stats.hasErrors()) {
-				printCompilationMessage('failure', port)
-			  } else {
-				printCompilationMessage('success', port)
-			  }
-			})
-		  })
-		}
-	  },
+	},
 
 	module: {
 		rules: [
@@ -65,14 +42,16 @@ module.exports = (_, argv) => ({
 
 	plugins: [
 		new ModuleFederationPlugin({
-			name: "roles",
+			name: "customer",
 			filename: "remoteEntry.js",
 			remotes: {
 				store: "store@https://store.admin.gworkspace.withhordanso.com/remoteEntry.js",
 			},
 			exposes: {
-				"./RoleApp": "./src/pages/index.tsx",
-			  },
+				"./Cart": "./src/components/Cart.tsx",
+				"./CustomerApp": "./src/pages/index.tsx",
+				"./Css": "./src/custom.css",
+			},
 			shared: {
 				...deps,
 				react: {
