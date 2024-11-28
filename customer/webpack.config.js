cae2eb4ee00c1ce9a51a2d4c539f081de5924e31
saunak99 <saunak99@gmail.com>
2024-11-28 17:54:42 +0530

    changes in customer file

--- customer/webpack.config.js
+++ customer/webpack.config.js
@@ -6,13 +6,13 @@ module.exports = (_, argv) => ({
 	output: {
 		publicPath: "auto",
 	},
-
+   
 	resolve: {
 		extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
 	},
 
 	devServer: {
-		port: 3001,
+		port: 4002,
 		historyApiFallback: true,
 		allowedHosts: ["all"],
 	},
