88d3c682a42b43fe83e4c266db3c465f0b85060c
saunak99 <saunak99@gmail.com>
2024-11-28 19:07:20 +0530

    changes in customer file

--- subscription/src/pages/index.tsx
+++ subscription/src/pages/index.tsx
@@ -1,33 +1,22 @@
 import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 
-const Header = React.lazy(() => import("../components/Heaader"));
-const Home = React.lazy(() => import("./Home"));
-const Login = React.lazy(() => import("./Login"));
-const Register = React.lazy(() => import("./Register"));
-const OTP = React.lazy(() => import("./OTP"));
-const ForgotPassword = React.lazy(() => import("./ForgotPassword"));
 
-const AuthApp: React.FC = () => {
+const PlanPriceSetup = React.lazy(() => import("./PlanPriceSetup"));
+const AddPlanPriceSetup = React.lazy(() => import("./AddPlanPriceSetup"));
+const GeminiSetup = React.lazy(() => import("./GeminiSetup"));
+
+
+const SubscriptionApp: React.FC = () => {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <Suspense fallback={<div>Loading...</div>}>
-          <Header />
-          <main className="py-10">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/otp" element={<OTP />} />
-              <Route path="/forgotpassword" element={<ForgotPassword />} />
-            </Routes>
-          </main>
-        </Suspense>
-      </div>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/plan-and-price-setup" element={<PlanPriceSetup />} />
+        <Route path="/add-plan-and-price-setup" element={<AddPlanPriceSetup />} />
+        <Route path="/gemini-setup" element={<GeminiSetup />} />
+      </Routes>
+    </Suspense>
   );
 };
 
-export default AuthApp;
+export default SubscriptionApp;
