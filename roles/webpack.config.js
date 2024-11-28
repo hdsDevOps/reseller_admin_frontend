6dbaed43d59bcdb5dc64c2c431a89c87112680b0
saunak99 <saunak99@gmail.com>
2024-11-28 18:02:05 +0530

    changes in customer file

--- roles/webpack.config.js
+++ roles/webpack.config.js
@@ -6,12 +6,12 @@ const Dotenv = require('dotenv-webpack');
 const deps = require("./package.json").dependencies;
 
 const printCompilationMessage = require('./compilation.config.js');
-  
+
 module.exports = (_, argv) => ({
   output: {
     publicPath: "auto",
   },
-  
+
   resolve: {
     extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
   },
@@ -65,7 +65,7 @@ module.exports = (_, argv) => ({
       name: "role",
       filename: "remoteEntry.js",
       remotes: {
-        store: "store@https://store.admin.gworkspace.withhordanso.com/remoteEntry.js",
+        store: `store@${process.env.STORE_BASE_URL || 'http://localhost:3030'}/remoteEntry.js`,
       },
       exposes: {
         "./RoleApp": "./src/pages/index.tsx",
