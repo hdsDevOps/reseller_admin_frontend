8aa915e37df039df91bbafbb73b1d4e506e325b3
saunak99 <saunak99@gmail.com>
2024-11-28 18:28:49 +0530

    changes in customer file

--- settings/webpack.config.js
+++ settings/webpack.config.js
@@ -15,7 +15,7 @@ module.exports = (_, argv) => ({
   resolve: {
     extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
   },
-
+   
   devServer: {
     port: 4007,
     historyApiFallback: true,
