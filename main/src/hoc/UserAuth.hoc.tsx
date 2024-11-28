3b693f7bdbbdb91e151b4832e75fa8237996a74d
saunak99 <saunak99@gmail.com>
2024-11-28 11:14:45 +0530

    changes in gitignore file

--- main/src/hoc/UserAuth.hoc.tsx
+++ main/src/hoc/UserAuth.hoc.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "store/hooks";
 import { authenticatedRoutes, unauthenticatedRoutes } from "../router";
@@ -9,62 +9,48 @@ interface UserAuthProps {
 }
 
 export default function UserAuth({ children }: UserAuthProps): JSX.Element {
-  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
   const location = useLocation();
   const dispatch = useAppDispatch();
   const token = useAppSelector((state) => state.auth.token);
-  // const token = ""
 
   useEffect(() => {
-    const getUserAuthToken = async () => {
-      try {
-        const result = await dispatch(getUserAuthTokenFromLSThunk()).unwrap();
-        if (result) {
-          // console.log("Token fetched:", result);
-        }
-      } catch (error) {
-        console.error("Error fetching token:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
+    //localStorage.clear();
     getUserAuthToken();
   }, [dispatch]);
 
   useEffect(() => {
-    if (loading) return; // Wait until loading is finished
-    const checkUserAuthStatus = () => {
-      const currentPath = location.pathname;
-      // console.log("Current path:", currentPath);
-      // console.log("Token:", token);
-
-      if (!token) {
-        if (unauthenticatedRoutes.includes(currentPath)) {
-          // console.log("Unauthenticated user on allowed path:", currentPath);
-          return; // Allow access to the unauthenticated page
-        } else {
-          // console.log("Unauthenticated user redirected to /home");
-          navigate("/login"); // Redirect unauthenticated users to the login page
-        }
-      } else {
-        if (authenticatedRoutes.includes(currentPath)) {
-          // console.log("Authenticated user on allowed path:", currentPath);
-          return; // Allow access to authenticated paths
-        } else if (!currentPath.startsWith("/dashboard")) {
-          // console.log("Authenticated user redirected to /dashboard");
-          navigate("/dashboard"); // Redirect authenticated users to the dashboard
-        }
-      }
-    };
-
     checkUserAuthStatus();
-  }, [token, location.pathname, loading, navigate]);
+  }, [token, location.pathname]);
 
-  if (loading) {
-    return <div>Loading...</div>; // Display a loading state while fetching token
-  }
+  const getUserAuthToken = async () => {
+    try {
+      const result = await dispatch(getUserAuthTokenFromLSThunk()).unwrap();
+      if (result) {
+        console.log("result", result);
+      }
+    } catch (error) {
+      console.error("Error fetching token:", error);
+      navigate("/login");
+    }
+  };
+
+  const checkUserAuthStatus = () => {
+    const currentPath = location.pathname;
+    if (!token) {
+      if (unauthenticatedRoutes.includes(currentPath)) {
+        return; // Allow access to the unauthenticated page
+      } else {
+        navigate("/login"); // Redirect unauthenticated users to the login page
+      }
+    } else {
+      if (authenticatedRoutes.includes(currentPath)) {
+        return; // Allow access to authenticated paths
+      } else if (!currentPath.startsWith("/dashboard")) {
+        navigate("/dashboard"); // Redirect authenticated users to the dashboard
+      }
+    }
+  };
 
   return <div>{children}</div>;
 }
