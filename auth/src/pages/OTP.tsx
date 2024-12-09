874f228ce03fae3af9a2c08bdd24da4804fc39d0
saunak99 <saunak99@gmail.com>
2024-12-09 16:06:57 +0530

    changes in main

--- auth/src/pages/OTP.tsx
+++ auth/src/pages/OTP.tsx
@@ -135,7 +135,7 @@ const OTP: React.FC = () => {
       <div className="max-w-md w-full space-y-8">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Sign in your account
+            Sign in your account 1111111
           </h2>
         </div>
         <form className="mt-8 space-y-6" onSubmit={handleLogin}>
