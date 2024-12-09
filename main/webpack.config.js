a31c60e94272bf6d01cdec9df6d2b6870cf17f7e
saunak99 <saunak99@gmail.com>
2024-12-09 12:08:19 +0530

    changes in main

--- main/webpack.config.js
+++ main/webpack.config.js
@@ -46,7 +46,7 @@ module.exports = (_, argv) => ({
 			filename: "remoteEntry.js",
 			remotes: {
 				store: "store@https://store.admin.gworkspace.withhordanso.com/remoteEntry.js",
-				auth: "auth@https://auth.admin.gworkspace.withhordanso.com/remoteEntry.js",
+				//auth: "auth@https://auth.admin.gworkspace.withhordanso.com/remoteEntry.js",
 				//customer: "customer@https://customer.admin.gworkspace.withhordanso.com/remoteEntry.js",
 			},
 			exposes: {
