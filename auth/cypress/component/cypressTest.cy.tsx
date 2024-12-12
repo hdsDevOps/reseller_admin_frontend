281c5ddba398ffa8123d126ea0a507abe8391cf7
saunak99 <saunak99@gmail.com>
2024-12-12 19:17:33 +0530

    changes

--- auth/cypress/component/cypressTest.cy.tsx
+++
@@ -1,8 +0,0 @@
-import PasswordResetSuccessful from '../../src/pages/PasswordResetSuccessful';
-import React from 'react';
-
-describe('Test the complete functionality', () => {
-  it('check everything is working', () => {
-    cy.mount(<PasswordResetSuccessful/>)
-  })
-})
