f8b46764b09e08c203ee2e9d989d48244356d69a
saunak99 <saunak99@gmail.com>
2024-12-16 12:33:14 +0530

    Deploy main

--- main/src/components/Footer.tsx
+++ main/src/components/Footer.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { useAppSelector } from "store/hooks";
 export default function Footer() {
+  const token = useAppSelector((state) => state.auth.token);
   return (
-    <footer
-      className="text-center bg-[#E3EFE5] border w-full font-inter font-normal text-[10px] text-[#848484] absolute left-0 right-0 bottom-0 z-[-1]"
-    >
-      <a>© 2024 HORDANSO WORKSPACE. All rights reserved</a>
+    <footer>
+      <div className="text-center p-3">
+        © 2024 Copyright:{token}
+        <a className="text-dark" href="#">
+          hordanso
+        </a>
+      </div>
     </footer>
   );
 }
