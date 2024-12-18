a96465613b47f748155c9a6847bb8e2bf3081fbf
saunak <saunak99@gmail.com>
2024-12-18 19:45:57 +0530

    latrst code deploy

--- main/src/App.tsx
+++ main/src/App.tsx
@@ -1,15 +1,59 @@
 // src/index.tsx
-import React from "react";
-import ReactDOM from "react-dom";  // Import ReactDOM to render to the DOM
-// Import your other necessary providers like ReduxProvider, BrowserRouter, etc.
-import { BrowserRouter } from "react-router-dom";
-// import { ReduxProvider } from './redux/store'; // Uncomment if using Redux
+import React, { Suspense, useEffect } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, useNavigate } from "react-router-dom";
+import ReduxProvider from "store/ReduxProvider";
+import UserAuth from "./hoc/UserAuth.hoc";
+import MainApp from "./pages";
+import AuthApp from "auth/AuthApp";
+import { useAppDispatch, useAppSelector } from "store/hooks";
+import "auth/AuthCss";
+import { getUserAuthTokenFromLSThunk, getUserIdFromLSThunk, getAdminDetailsThunk, getDefaultCurrencyThunk } from "store/user.thunk";
 
 const App: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const { token } = useAppSelector((state) => state.auth);
+  // const token = "usy6767jshs688ytmbqa88654sgsgs5sgs6sgs6q";
+  // const token = "";
+  // console.log(token, 'token');
+  const { userId } = useAppSelector((state) => state.auth);
+  const { defaultCurrency } = useAppSelector((state) => state.auth);
+  
+  
+
+  useEffect(() => {
+    const getUserAuthToken = async () => {
+      try {
+        await dispatch(getUserAuthTokenFromLSThunk()).unwrap();
+        await dispatch(getUserIdFromLSThunk()).unwrap();
+      } catch (error) {
+        // console.error("Error fetching token:", error);
+        navigate('/login');
+      }
+    };
+
+    getUserAuthToken();
+  }, [token, dispatch, navigate]);
+
+  useEffect(() => {
+    const fetchUserDetails = async() => {
+      try {
+        const getUserIdFromLocalStorage = await dispatch(getUserIdFromLSThunk()).unwrap();
+        await dispatch(getAdminDetailsThunk({userid: getUserIdFromLocalStorage})).unwrap();
+        await dispatch(getDefaultCurrencyThunk({userid: getUserIdFromLocalStorage})).unwrap();
+      } catch (error) {
+        //
+      }
+    };
+
+    fetchUserDetails();
+  }, [userId]);
+
   return (
-    <div>
-      <h2>Loading.....</h2>
-    </div>
+    <Suspense fallback={<h2>Loading.....</h2>}>
+      {token ? <MainApp /> : <AuthApp />}
+    </Suspense>
   );
 };
 
@@ -18,9 +62,13 @@ if (!rootElement) throw new Error("Failed to find the root element");
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ReduxProvider>
+      <BrowserRouter>
+        <UserAuth>
+          <App />
+        </UserAuth>
+      </BrowserRouter>
+    </ReduxProvider>
   </React.StrictMode>,
   rootElement
 );
