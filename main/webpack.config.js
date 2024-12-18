5b57b6e0d9f2cf2bc33eab6a527a1bb04dbbc1b0
Prosenjit Dutta <prosenjit.dutta@schemaphic.com>
2024-12-18 18:32:35 +0530

    Code update

--- main/webpack.config.js
+++ main/webpack.config.js
@@ -1,12 +1,7 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const path = require('path');
-const Dotenv = require('dotenv-webpack');
-
+const Dotenv = require("dotenv-webpack");
 const deps = require("./package.json").dependencies;
-
-const printCompilationMessage = require('./compilation.config.js');
-
 module.exports = (_, argv) => ({
   output: {
     publicPath: "auto",
@@ -15,7 +10,7 @@ module.exports = (_, argv) => ({
   resolve: {
     extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
   },
-
+   
   devServer: {
     port: 4000,
     historyApiFallback: true,
@@ -50,9 +45,19 @@ module.exports = (_, argv) => ({
       name: "main",
       filename: "remoteEntry.js",
       remotes: {
-        store: `store@${process.env.STORE_BASE_URL || 'http://localhost:4030'}/remoteEntry.js`
+        store: `store@${process.env.STORE_BASE_URL || 'http://localhost:4030'}/remoteEntry.js`,
+        auth: `auth@${process.env.AUTH_BASE_URL || 'http://localhost:4002'}/remoteEntry.js`,
+        customer: `customer@${process.env.CUSTOMER_BASE_URL || 'http://localhost:4001'}/remoteEntry.js`,
+        paymenthistory: `paymenthistory@${process.env.PAYMENT_BASE_URL || 'http://localhost:4005'}/remoteEntry.js`,
+        role: `role@${process.env.ROLE_BASE_URL || 'http://localhost:4006'}/remoteEntry.js`,
+        settings: `settings@${process.env.SETTINGS_BASE_URL || 'http://localhost:4007'}/remoteEntry.js`,
+        subscription: `subscription@${process.env.SUBSCRIPTION_BASE_URL || 'http://localhost:4004'}/remoteEntry.js`,
+        vouchernotification: `vouchernotification@${process.env.VOUCHER_BASE_URL || 'http://localhost:4003'}/remoteEntry.js`,
       },
-      exposes: {},
+      exposes: {
+        "./Navbar": "./src/components/Navbar.tsx",
+        "./Header": "./src/components/Header.tsx",
+      },
       shared: {
         ...deps,
         react: {
@@ -68,6 +73,6 @@ module.exports = (_, argv) => ({
     new HtmlWebPackPlugin({
       template: "./src/index.html",
     }),
-    new Dotenv()
+    new Dotenv(),
   ],
 });
