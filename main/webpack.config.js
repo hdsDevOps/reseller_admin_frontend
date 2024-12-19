f9674f7a2f7a111e81b8a164d3101240788052c3
saunak <saunak99@gmail.com>
2024-12-19 10:48:35 +0530

    latrst code deploy

--- main/webpack.config.js
+++ main/webpack.config.js
@@ -47,11 +47,11 @@ module.exports = (_, argv) => ({
       remotes: {
         // store: `store@https://store.customer.gworkspace.withhordanso.com/remoteEntry.js`,
         // auth: `auth@https://auth.customer.gworkspace.withhordanso.com/remoteEntry.js`,
-        // domains: `domains@https://domain.customer.gworkspace.withhordanso.com/remoteEntry.js`,
-        // billinghistory: `billinghistory@https://billinghistory.customer.gworkspace.withhordanso.com/remoteEntry.js`,
-        // payments: `payments@https://payments.customer.gworkspace.withhordanso.com/remoteEntry.js`,
-        // settings: `settings@https://settings.customer.gworkspace.withhordanso.com/remoteEntry.js`,
-        // email: `email@https://email.customer.gworkspace.withhordanso.com/remoteEntry.js`,
+         domains: `domains@https://domain.customer.gworkspace.withhordanso.com/remoteEntry.js`,
+         billinghistory: `billinghistory@https://billinghistory.customer.gworkspace.withhordanso.com/remoteEntry.js`,
+         payments: `payments@https://payments.customer.gworkspace.withhordanso.com/remoteEntry.js`,
+         settings: `settings@https://settings.customer.gworkspace.withhordanso.com/remoteEntry.js`,
+         email: `email@https://email.customer.gworkspace.withhordanso.com/remoteEntry.js`,
 
         store: "store@https://store.admin.gworkspace.withhordanso.com/remoteEntry.js",
 				auth: "auth@https://authtest.admin.gworkspace.withhordanso.com/remoteEntry.js",
